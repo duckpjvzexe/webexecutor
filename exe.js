25521332517ab251aaf311e47cbe877aa33df9c2
duckpjvzexe <hoangconghung398@gmail.com>
2026-02-07 05:54:11 +0700

    Update exe.js

--- exe.js
+++ exe.js
@@ -6,7 +6,7 @@ const data = [
   {id:2,name:'Arceus X NEO',rating:4.5,avatar:'https://files.catbox.moe/y8ddt5.png',version:'2.668.658',versionVng:null,status:'offline',statusVng:null,platforms:['ios'],downloadUrl:'https://ios.spdmteam.com/',downloadVngUrl:null},
 
   // Codex
-  {id:3,name:'Codex',rating:4.8,avatar:'https://files.catbox.moe/s6hbqj.png',version:'2.705.924',versionVng:'2.703.1353',status:'online',statusVng:'offline',platforms:['android'],downloadUrl:'https://www.mediafire.com/file/qnzbaba4c8rjncw/Codex+V2.705.924.apk/file',downloadVngUrl:'https://www.ngocbonggaming.com/2025/09/link-tai-codex-vng-androi.html'},
+  {id:3,name:'Codex',rating:4.8,avatar:'https://files.catbox.moe/s6hbqj.png',version:'2.706.750',versionVng:'2.703.1353',status:'online',statusVng:'offline',platforms:['android'],downloadUrl:'https://www.mediafire.com/file/v2wqjrqpgttq4fs/Codex+v2.706.750.apk/file',downloadVngUrl:'https://www.ngocbonggaming.com/2025/09/link-tai-codex-vng-androi.html'},
   {id:4,name:'Codex',rating:4.8,avatar:'https://files.catbox.moe/s6hbqj.png',version:'2.674.782',versionVng:null,status:'offline',statusVng:null,platforms:['ios'],downloadUrl:'https://www.mediafire.com/file/7afzer5xxo1110n/Codex_2.674.782.ipa/file',downloadVngUrl:null},
 
   // Cryptic
@@ -24,7 +24,7 @@ const data = [
   {id:10,name:'Trigon',rating:4.5,avatar:'https://img.upanh.moe/ynRPDwBx/81e69e89eab4.webp',version:'2.705.924',versionVng:'2.704.1057',status:'online',statusVng:'online',platforms:['android'],downloadUrl:'https://trigonevo.com/download?os=android&fs=Mediafire',downloadVngUrl:'https://www.mediafire.com/file/vpc8fy1taim2k22/Trigon+VNG+32Bit+&+64Bit.apk/file'},
 
   // Vega X
-  {id:11,name:'Vega X',rating:4.4,avatar:'https://vegax.gg/assets/img/V_logo_white.png',version:'2.705.924',versionVng:null,status:'online',statusVng:null,platforms:['android'],downloadUrl:'https://github.com/1f0yt/community/releases',downloadVngUrl:null},
+  {id:11,name:'Vega X',rating:4.4,avatar:'https://vegax.gg/assets/img/V_logo_white.png',version:'2.706.750',versionVng:null,status:'online',statusVng:null,platforms:['android'],downloadUrl:'https://github.com/1f0yt/community/releases',downloadVngUrl:null},
 
   // ===== PC (Windows) =====
 

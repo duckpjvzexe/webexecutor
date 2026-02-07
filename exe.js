dc80bfae60a6d3855e19c475b94da5ebd157ad64
duckpjvzexe <hoangconghung398@gmail.com>
2026-02-07 19:27:28 +0700

    Update exe.js

--- exe.js
+++ exe.js
@@ -29,7 +29,7 @@ const data = [
   // ===== PC (Windows) =====
 
   {id:12,name:'Bunni.lol',rating:4.6,avatar:'https://files.catbox.moe/hd9264.webp',version:'db4634f0e27d4d36',versionVng:null,status:'offline',statusVng:null,platforms:['windows'],downloadUrl:'https://bunni.fun/',downloadVngUrl:null},
-  {id:13,name:'Velocity',rating:4.4,avatar:'https://files.catbox.moe/tj6brv.png',version:'db4634f0e27d4d36',versionVng:null,status:'offline',statusVng:null,platforms:['windows'],downloadUrl:'https://gofile.io/d/biQGwp',downloadVngUrl:null},
+  {id:13,name:'Velocity',rating:4.4,avatar:'https://files.catbox.moe/tj6brv.png',version:'80c7b8e578f241ff',versionVng:null,status:'online',statusVng:null,platforms:['windows'],downloadUrl:'https://gofile.io/d/biQGwp',downloadVngUrl:null},
   {id:14,name:'Swift',rating:4.8,avatar:'https://files.catbox.moe/5krpqt.webp',version:'89b6c70d8f7f42ac',versionVng:null,status:'offline',statusVng:null,platforms:['windows'],downloadUrl:'https://getswift.vip/',downloadVngUrl:null},
   {id:15,name:'Trigon',rating:4.2,avatar:'https://img.upanh.moe/ynRPDwBx/81e69e89eab4.webp',version:'db4634f0e27d4d36',versionVng:null,status:'offline',statusVng:null,platforms:['windows'],downloadUrl:'https://trigonevo.com/download?os=windows&fs=mediafire',downloadVngUrl:null}
 ];

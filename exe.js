3e689817dc24c1405e3b0d20eb2112a6c0fd84ba
duckpjvzexe <hoangconghung398@gmail.com>
2026-03-04 18:21:06 +0700

    Update exe.js

--- exe.js
+++ exe.js
@@ -21,10 +21,10 @@ const data = [
   {id:9,name:'Trigon',rating:4.5,avatar:'https://img.upanh.moe/ynRPDwBx/81e69e89eab4.webp',version:'2.708.880',versionVng:'2.708.880',status:'offline',statusVng:'online',platforms:['android'],downloadUrl:'https://trigonevo.com/download?os=android&fs=Mediafire',downloadVngUrl:'https://www.mediafire.com/file/cyd1thmejxujgdp/Trigon+VNG+V2.708.apk/file'},
 
   // Vega X
-  {id:10,name:'Vega X',rating:4.4,avatar:'https://vegax.gg/assets/img/V_logo_white.png',version:'2.708.880',versionVng:null,status:'online',statusVng:null,platforms:['android'],downloadUrl:'https://github.com/1f0yt/community/releases',downloadVngUrl:null},
+  {id:10,name:'Vega X',rating:4.4,avatar:'https://vegax.gg/assets/img/V_logo_white.png',version:'2.708.880',versionVng:null,status:'offline',statusVng:null,platforms:['android'],downloadUrl:'https://github.com/1f0yt/community/releases',downloadVngUrl:null},
   
   // Ronix
-  {id:11,name:'Ronix',rating:4.5,avatar:'https://obj.wearedevs.net/images/software/ronix/ronix-020426.webp',version:'2.709.873',versionVng:'2.707.735',status:'online',statusVng:'online',platforms:['android'],downloadUrl:'https://wrdcdn.net/r/154522/1772529634821/Ronix_64Bits-2.709.873.apk',downloadVngUrl:'https://wrdcdn.net/r/154522/1771267795196/Ronix_VNG_64Bits-2.707.735.apk'},
+  {id:11,name:'Ronix',rating:4.5,avatar:'https://obj.wearedevs.net/images/software/ronix/ronix-020426.webp',version:'2.709.873',versionVng:'2.707.735',status:'offline',statusVng:'offline',platforms:['android'],downloadUrl:'https://wrdcdn.net/r/154522/1772529634821/Ronix_64Bits-2.709.873.apk',downloadVngUrl:'https://wrdcdn.net/r/154522/1771267795196/Ronix_VNG_64Bits-2.707.735.apk'},
   {id:12,name:'Ronix',rating:4.5,avatar:'https://obj.wearedevs.net/images/software/ronix/ronix-020426.webp',version:'2.709.873',versionVng:'2.709.873',status:'online',statusVng:'online',platforms:['ios'],downloadUrl:'https://wrdcdn.net/r/154522/1771864141368/Ronix_2.709.869-rel.ipa',downloadVngUrl:'https://wrdcdn.net/r/154522/1771864203342/RonixVN_NEW.ipa'},
   
   // ===== PC (Windows) =====

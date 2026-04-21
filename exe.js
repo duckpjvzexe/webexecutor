7aa502bc6593a286268eb2ca7ef80b40d61dabf0
duckpjvzexe <hoangconghung398@gmail.com>
2026-04-21 20:46:15 +0700

    Update exe.js

--- exe.js
+++ exe.js
@@ -6,7 +6,7 @@ const data = [
   {id:2,name:'Arceus X NEO',rating:4.5,avatar:'https://files.catbox.moe/6l8y21.webp',version:'2.668.658',versionVng:null,status:'offline',statusVng:null,platforms:['ios'],downloadUrl:'https://ios.spdmteam.com/',downloadVngUrl:null},
 
   // Codex
-  {id:3,name:'Codex',rating:4.8,avatar:'https://files.catbox.moe/t43q3r.webp',version:'2.715.1115',versionVng:'2.715.1115',status:'offline',statusVng:'online',platforms:['android'],downloadUrl:'https://www.mediafire.com/file/qpitgkaqtwfj9st/Codex+v2.715.apk/file',downloadVngUrl:'https://www.mediafire.com/file/5p08n8qhtsv4x3s/Codex+VNG+V2.715+No+Key.apk/file'},
+  {id:3,name:'Codex',rating:4.8,avatar:'https://files.catbox.moe/t43q3r.webp',version:'2.716.875',versionVng:'2.715.1115',status:'online',statusVng:'online',platforms:['android'],downloadUrl:'https://www.mediafire.com/file/4cadggxj7k5vktx/Codex+v2.716.apk/file',downloadVngUrl:'https://www.mediafire.com/file/5p08n8qhtsv4x3s/Codex+VNG+V2.715+No+Key.apk/file'},
   {id:4,name:'Codex',rating:4.8,avatar:'https://files.catbox.moe/t43q3r.webp',version:'2.674.782',versionVng:null,status:'offline',statusVng:null,platforms:['ios'],downloadUrl:'https://www.mediafire.com/file/7afzer5xxo1110n/Codex_2.674.782.ipa/file',downloadVngUrl:null},
 
   // Cryptic

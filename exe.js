e902949ebdec1b2a98681a43b366af78a89df27a
duckpjvzexe <hoangconghung398@gmail.com>
2026-06-24 22:21:08 +0700

    Update exe.js

--- exe.js
+++ exe.js
@@ -10,6 +10,6 @@ const DATA = [
   
   {name:"Vega X",rating:4.4,avatar:"https://vegax.gg/assets/img/V_logo_white.png",version:"2.724.735",versionVng:null,status:"online",statusVng:null,platforms:["android"],downloadUrl:"https://github.com/1f0yt/community/releases",downloadVngUrl:null},
   
-  {name:"Velocity",rating:4.4,avatar:"https://files.catbox.moe/tj6brv.png",version:"8884371d30284041",versionVng:null,status:"online",statusVng:null,platforms:["windows"],downloadUrl:"https://gofile.io/d/XxqkNV",downloadVngUrl:null},
+  {name:"Velocity",rating:4.4,avatar:"https://files.catbox.moe/tj6brv.png",version:"8884371d30284041",versionVng:null,status:"offline",statusVng:null,platforms:["windows"],downloadUrl:"https://gofile.io/d/XxqkNV",downloadVngUrl:null},
   {name:"Madium",rating:4.6,avatar:"https://files.catbox.moe/c6crdn.webp",version:"8884371d30284041",versionVng:null,status:"online",statusVng:null,platforms:["windows"],downloadUrl:"https://filerift.com/file/SPx-uFMDag",downloadVngUrl:null},
 ];

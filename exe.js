604f4ccd819dea33d7b7f3b14dea4dd52e28b759
duckpjvzexe <hoangconghung398@gmail.com>
2026-06-26 08:38:24 +0700

    Update exe.js

--- exe.js
+++ exe.js
@@ -1,9 +1,9 @@
 const DATA = [
-  {name:"Arceus X NEO",rating:4.5,avatar:"https://files.catbox.moe/6l8y21.webp",version:"2.724.735",versionVng:"2.716.875",status:"online",statusVng:"offline",platforms:["android"],downloadUrl:"https://android.spdmteam.com/",downloadVngUrl:"https://vng.spdmteam.com/"},
+  {name:"Arceus X NEO",rating:4.5,avatar:"https://files.catbox.moe/6l8y21.webp",version:"2.725.1142",versionVng:"2.716.875",status:"online",statusVng:"offline",platforms:["android"],downloadUrl:"https://android.spdmteam.com/",downloadVngUrl:"https://vng.spdmteam.com/"},
   
   {name:"Codex",rating:4.8,avatar:"https://files.catbox.moe/t43q3r.webp",version:"2.724.735",versionVng:"2.724.735",status:"online",statusVng:"online",platforms:["android"],downloadUrl:"https://www.mediafire.com/file/mjuct6hu6v31u42/Codex+v2.724.735.apk/file",downloadVngUrl:"https://bom.so/fOuVvg"},
   
-  {name:"Delta",rating:4.9,avatar:"https://deltaexploits.gg/assets/favicon.ico",version:"2.724.735",versionVng:"2.724.735",status:"online",statusVng:"online",platforms:["android"],downloadUrl:"https://gloopup.net/Delta/android/",downloadVngUrl:"https://bom.so/MRlFOh"},
+  {name:"Delta",rating:4.9,avatar:"https://deltaexploits.gg/assets/favicon.ico",version:"2.725.1142",versionVng:"2.724.735",status:"online",statusVng:"online",platforms:["android"],downloadUrl:"https://gloopup.net/Delta/android/",downloadVngUrl:"https://bom.so/MRlFOh"},
   {name:"Delta",rating:4.9,avatar:"https://deltaexploits.gg/assets/favicon.ico",version:"2.725.1142",versionVng:"2.725.1142",status:"online",statusVng:"online",platforms:["ios"],downloadUrl:"https://cdn.glopdelivery.com/file/Delta-2.725.1142.ipa",downloadVngUrl:"https://bom.so/xWHPyK"},
   
   {name:"Trigon",rating:4.5,avatar:"https://img.upanh.moe/ynRPDwBx/81e69e89eab4.webp",version:"2.724.735",versionVng:null,status:"online",statusVng:null,platforms:["android"],downloadUrl:"https://trigonevo.com/download?os=android&fs=mediafire",downloadVngUrl:null},

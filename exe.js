f0265939af1a68a598f31e6e279de125380f4131
duckpjvzexe <hoangconghung398@gmail.com>
2026-07-18 19:06:50 +0700

    Update exe.js

--- exe.js
+++ exe.js
@@ -1,15 +1,15 @@
 const DATA = [
-  {name:"Arceus X NEO",rating:4.5,avatar:"https://files.catbox.moe/6l8y21.webp",version:"2.726.1142",versionVng:"2.716.875",status:"online",statusVng:"offline",platforms:["android"],downloadUrl:"https://android.spdmteam.com/",downloadVngUrl:"https://vng.spdmteam.com/"},
+  {name:"Arceus X NEO",rating:4.5,avatar:"https://files.catbox.moe/6l8y21.webp",version:"2.729.840",versionVng:"2.716.875",status:"online",statusVng:"offline",platforms:["android"],downloadUrl:"https://android.spdmteam.com/",downloadVngUrl:"https://vng.spdmteam.com/"},
   
-  {name:"Codex",rating:4.8,avatar:"https://files.catbox.moe/t43q3r.webp",version:"2.726.1142",versionVng:"2.724.735",status:"online",statusVng:"online",platforms:["android"],downloadUrl:"https://bom.so/YWfmg2",downloadVngUrl:"https://bom.so/fOuVvg"},
+  {name:"Codex",rating:4.8,avatar:"https://files.catbox.moe/t43q3r.webp",version:"2.729.840",versionVng:null,status:"online",statusVng:null,platforms:["android"],downloadUrl:"https://bom.so/uB4FEU",downloadVngUrl:null},
   
   {name:"Delta",rating:4.9,avatar:"https://deltaexploits.gg/assets/favicon.ico",version:"2.727.1199",versionVng:"2.727.1199",status:"online",statusVng:"online",platforms:["android"],downloadUrl:"https://gloopup.net/Delta/android/",downloadVngUrl:"https://bom.so/ohSvJQ"},
-  {name:"Delta",rating:4.9,avatar:"https://deltaexploits.gg/assets/favicon.ico",version:"2.727.1199",versionVng:"2.725.1142",status:"online",statusVng:"online",platforms:["ios"],downloadUrl:"https://cdn.glopdelivery.com/file/Delta-2.727.1199.ipa",downloadVngUrl:"https://bom.so/xWHPyK"},
+  {name:"Delta",rating:4.9,avatar:"https://deltaexploits.gg/assets/favicon.ico",version:"2.727.1199",versionVng:"2.727.1198",status:"online",statusVng:"online",platforms:["ios"],downloadUrl:"https://cdn.glopdelivery.com/file/Delta-2.727.1199.ipa",downloadVngUrl:"https://bom.so/0niUJ5"},
   
   {name:"Trigon",rating:4.5,avatar:"https://img.upanh.moe/ynRPDwBx/81e69e89eab4.webp",version:"2.726.1142",versionVng:null,status:"online",statusVng:null,platforms:["android"],downloadUrl:"https://trigonevo.com/download?os=android&fs=mediafire",downloadVngUrl:null},
   
   {name:"Vega X",rating:4.4,avatar:"https://vegax.gg/assets/img/V_logo_white.png",version:"2.726.1142",versionVng:null,status:"online",statusVng:null,platforms:["android"],downloadUrl:"https://github.com/1f0yt/community/releases",downloadVngUrl:null},
   
-  {name:"Velocity",rating:4.4,avatar:"https://files.catbox.moe/tj6brv.png",version:"36a2600cebf1487d",versionVng:null,status:"online",statusVng:null,platforms:["windows"],downloadUrl:"https://gofile.io/d/6HAQxH",downloadVngUrl:null},
-  {name:"Madium",rating:4.6,avatar:"https://files.catbox.moe/c6crdn.webp",version:"36a2600cebf1487d",versionVng:null,status:"online",statusVng:null,platforms:["windows"],downloadUrl:"https://filerift.com/file/BEN2BKv00w",downloadVngUrl:null},
+  {name:"Velocity",rating:4.4,avatar:"https://files.catbox.moe/tj6brv.png",version:"ddf02245bdbb428c",versionVng:null,status:"online",statusVng:null,platforms:["windows"],downloadUrl:"https://gofile.io/d/6HAQxH",downloadVngUrl:null},
+  {name:"Madium",rating:4.6,avatar:"https://files.catbox.moe/c6crdn.webp",version:"ddf02245bdbb428c",versionVng:null,status:"online",statusVng:null,platforms:["windows"],downloadUrl:"https://filerift.com/file/BEN2BKv00w",downloadVngUrl:null},
 ];

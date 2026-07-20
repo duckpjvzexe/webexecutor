604ec89c478e3e2d6dc163857f46b2a26fd28750
duckpjvzexe <hoangconghung398@gmail.com>
2026-07-20 11:39:49 +0700

    Update exe.js

--- exe.js
+++ exe.js
@@ -3,12 +3,12 @@ const DATA = [
   
   {name:"Codex",rating:4.8,avatar:"https://files.catbox.moe/t43q3r.webp",version:"2.729.840",versionVng:null,status:"online",statusVng:null,platforms:["android"],downloadUrl:"https://bom.so/uB4FEU",downloadVngUrl:null},
   
-  {name:"Delta",rating:4.9,avatar:"https://deltaexploits.gg/assets/favicon.ico",version:"2.727.1199",versionVng:"2.727.1199",status:"online",statusVng:"online",platforms:["android"],downloadUrl:"https://gloopup.net/Delta/android/",downloadVngUrl:"https://bom.so/ohSvJQ"},
+  {name:"Delta",rating:4.9,avatar:"https://deltaexploits.gg/assets/favicon.ico",version:"2.729.840",versionVng:"2.729.840",status:"online",statusVng:"online",platforms:["android"],downloadUrl:"https://gloopup.net/Delta/android/",downloadVngUrl:"https://bom.so/ajoX3P"},
   {name:"Delta",rating:4.9,avatar:"https://deltaexploits.gg/assets/favicon.ico",version:"2.727.1199",versionVng:"2.727.1198",status:"online",statusVng:"online",platforms:["ios"],downloadUrl:"https://cdn.glopdelivery.com/file/Delta-2.727.1199.ipa",downloadVngUrl:"https://bom.so/0niUJ5"},
   
   {name:"Trigon",rating:4.5,avatar:"https://img.upanh.moe/ynRPDwBx/81e69e89eab4.webp",version:"2.726.1142",versionVng:null,status:"online",statusVng:null,platforms:["android"],downloadUrl:"https://trigonevo.com/download?os=android&fs=mediafire",downloadVngUrl:null},
   
-  {name:"Vega X",rating:4.4,avatar:"https://vegax.gg/assets/img/V_logo_white.png",version:"2.726.1142",versionVng:null,status:"online",statusVng:null,platforms:["android"],downloadUrl:"https://github.com/1f0yt/community/releases",downloadVngUrl:null},
+  {name:"Vega X",rating:4.4,avatar:"https://vegax.gg/assets/img/V_logo_white.png",version:"2.729.840",versionVng:null,status:"online",statusVng:null,platforms:["android"],downloadUrl:"https://github.com/1f0yt/community/releases",downloadVngUrl:null},
   
   {name:"Velocity",rating:4.4,avatar:"https://files.catbox.moe/tj6brv.png",version:"ddf02245bdbb428c",versionVng:null,status:"online",statusVng:null,platforms:["windows"],downloadUrl:"https://gofile.io/d/6HAQxH",downloadVngUrl:null},
   {name:"Madium",rating:4.6,avatar:"https://files.catbox.moe/c6crdn.webp",version:"ddf02245bdbb428c",versionVng:null,status:"online",statusVng:null,platforms:["windows"],downloadUrl:"https://filerift.com/file/BEN2BKv00w",downloadVngUrl:null},

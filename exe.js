fada807ba3c57aa9e05c31c03fce7244631f1edd
duckpjvzexe <hoangconghung398@gmail.com>
2026-07-21 09:51:24 +0700

    Update exe.js

--- exe.js
+++ exe.js
@@ -4,7 +4,7 @@ const DATA = [
   {name:"Codex",rating:4.8,avatar:"https://files.catbox.moe/t43q3r.webp",version:"2.729.840",versionVng:null,status:"online",statusVng:null,platforms:["android"],downloadUrl:"https://bom.so/uB4FEU",downloadVngUrl:null},
   
   {name:"Delta",rating:4.9,avatar:"https://deltaexploits.gg/assets/favicon.ico",version:"2.729.840",versionVng:"2.729.840",status:"online",statusVng:"online",platforms:["android"],downloadUrl:"https://gloopup.net/Delta/android/",downloadVngUrl:"https://bom.so/ajoX3P"},
-  {name:"Delta",rating:4.9,avatar:"https://deltaexploits.gg/assets/favicon.ico",version:"2.727.1199",versionVng:"2.727.1198",status:"online",statusVng:"online",platforms:["ios"],downloadUrl:"https://cdn.glopdelivery.com/file/Delta-2.727.1199.ipa",downloadVngUrl:"https://bom.so/0niUJ5"},
+  {name:"Delta",rating:4.9,avatar:"https://deltaexploits.gg/assets/favicon.ico",version:"2.729.838",versionVng:"2.729.838",status:"online",statusVng:"online",platforms:["ios"],downloadUrl:"https://cdn.glopdelivery.com/file/Delta-2.729.838.ipa",downloadVngUrl:"https://ipas.cc/g15fmx"},
   
   {name:"Trigon",rating:4.5,avatar:"https://img.upanh.moe/ynRPDwBx/81e69e89eab4.webp",version:"2.726.1142",versionVng:null,status:"online",statusVng:null,platforms:["android"],downloadUrl:"https://trigonevo.com/download?os=android&fs=mediafire",downloadVngUrl:null},
   

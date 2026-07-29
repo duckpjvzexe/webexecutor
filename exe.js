bcfb08c28504724cd1122194f75c96bc167e70c6
duckpjvzexe <hoangconghung398@gmail.com>
2026-07-29 13:27:05 +0700

    Update exe.js

--- exe.js
+++ exe.js
@@ -10,6 +10,7 @@ const DATA = [
   
   {name:"Vega X",rating:4.4,avatar:"https://vegax.gg/assets/img/V_logo_white.png",version:"2.730.790",versionVng:null,status:"online",statusVng:null,platforms:["android"],downloadUrl:"https://github.com/1f0yt/community/releases",downloadVngUrl:null},
   
-  {name:"Velocity",rating:4.4,avatar:"https://files.catbox.moe/tj6brv.png",version:"9affbe66b2624d20",versionVng:null,status:"online",statusVng:null,platforms:["windows"],downloadUrl:"https://gofile.io/d/6HAQxH",downloadVngUrl:null},
-  {name:"Madium",rating:4.6,avatar:"https://files.catbox.moe/c6crdn.webp",version:"9affbe66b2624d20",versionVng:null,status:"online",statusVng:null,platforms:["windows"],downloadUrl:"https://filerift.com/file/BEN2BKv00w",downloadVngUrl:null},
+  {name:"Velocity",rating:4.4,avatar:"https://files.catbox.moe/tj6brv.png",version:"145f189a6a974303",versionVng:null,status:"online",statusVng:null,platforms:["windows"],downloadUrl:"https://gofile.io/d/6HAQxH",downloadVngUrl:null},
+  {name:"Madium",rating:4.6,avatar:"https://files.catbox.moe/c6crdn.webp",version:"145f189a6a974303",versionVng:null,status:"online",statusVng:null,platforms:["windows"],downloadUrl:"https://filerift.com/file/BEN2BKv00w",downloadVngUrl:null},
+  {name:"Real",rating:4.6,avatar:"https://sf-static.upanhlaylink.com/img/image_202607297d93e97f204c30b33349e861fb32f1e9.jpg",version:"145f189a6a974303",versionVng:null,status:"online",statusVng:null,platforms:["windows"],downloadUrl:"https://projectreal.gg/download/",downloadVngUrl:null},
 ];

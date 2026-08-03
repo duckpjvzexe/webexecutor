c65d9d63fadece8aba861d78c3d42bd871b29eda
duckpjvzexe <hoangconghung398@gmail.com>
2026-08-03 16:39:24 +0700

    Update exe.js

--- exe.js
+++ exe.js
@@ -1,7 +1,7 @@
 const DATA = [
   {name:"Arceus X NEO",rating:4.5,avatar:"https://files.catbox.moe/6l8y21.webp",version:"2.730.790",versionVng:"2.716.875",status:"online",statusVng:"offline",platforms:["android"],downloadUrl:"https://android.spdmteam.com/",downloadVngUrl:"https://vng.spdmteam.com/"},
   
-  {name:"Codex",rating:4.8,avatar:"https://files.catbox.moe/t43q3r.webp",version:"2.729.840",versionVng:null,status:"online",statusVng:null,platforms:["android"],downloadUrl:"https://bom.so/uB4FEU",downloadVngUrl:null},
+  {name:"Codex",rating:4.8,avatar:"https://files.catbox.moe/t43q3r.webp",version:"2.731.944",versionVng:null,status:"online",statusVng:null,platforms:["android"],downloadUrl:"https://bom.so/0yETOR",downloadVngUrl:null},
   
   {name:"Delta",rating:4.9,avatar:"https://deltaexploits.gg/assets/favicon.ico",version:"2.731.944",versionVng:"2.731.944",status:"online",statusVng:"online",platforms:["android"],downloadUrl:"https://gloopup.net/Delta/android/",downloadVngUrl:"https://bom.so/AxdZLe"},
   {name:"Delta",rating:4.9,avatar:"https://deltaexploits.gg/assets/favicon.ico",version:"2.729.838",versionVng:"2.730.790",status:"online",statusVng:"online",platforms:["ios"],downloadUrl:"https://cdn.glopdelivery.com/file/Delta-2.729.838.ipa",downloadVngUrl:"https://ipas.cc/ieptp2"},
